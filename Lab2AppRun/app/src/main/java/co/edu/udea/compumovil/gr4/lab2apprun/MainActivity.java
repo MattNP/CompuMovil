531aed6004d411ea23986742516333094dc76a11
Sergio Marriaga <sergio.marriaga@udea.edu.co>
2016-03-11 15:10:35 -0500

    Commit

---
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr4/lab2apprun/MainActivity.java
@@ -0,0 +1,19 @@
+package co.edu.udea.compumovil.gr4.lab2apprun;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Lanzar(View v){
+        Intent intent= new Intent(this,Registro.class);
+        startActivity(intent);
+    }
+}
