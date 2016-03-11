47259224cd52643668f7bc6f7f94ac453ad1e34c
Sergio Marriaga <sergio.marriaga@udea.edu.co>
2016-03-11 16:36:27 -0500

    eventos

--- Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr4/lab2apprun/MainActivity.java
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr4/lab2apprun/MainActivity.java
@@ -3,17 +3,47 @@ package co.edu.udea.compumovil.gr4.lab2apprun;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST=1;
+    static final String USER="USER";
+    static final String CLAVE="CLAVE";
+    private EditText user, clave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user= (EditText) findViewById(R.id.usuario);
+        clave=(EditText) findViewById(R.id.clave);
+
     }
     public void Lanzar(View v){
         Intent intent= new Intent(this,Registro.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST){
+
+            String userL=data.getStringExtra(USER);
+            String claveL=data.getStringExtra(CLAVE);
+            user.setText(userL);
+            clave.setText(claveL);
+
+
+        }
+    }
+    public void ingresar(View v){
+        Intent intent =new Intent(this,Eventos.class);
+        startActivityForResult(intent,REQUEST);
+
     }
 }
--- Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr4/lab2apprun/Registro.java
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr4/lab2apprun/Registro.java
@@ -1,13 +1,33 @@
 package co.edu.udea.compumovil.gr4.lab2apprun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
+    private EditText usuario,clave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        usuario = (EditText)findViewById(R.id.usuarioR);
+        clave = (EditText)findViewById(R.id.claveR);
+    }
+    public void volver(View v){
+
+        String usuarioR = usuario.getText().toString();
+        String claveR=clave.getText().toString();
+        Intent intent=new Intent();
+        intent.putExtra(MainActivity.USER, usuarioR);
+        intent.putExtra(MainActivity.CLAVE, claveR);
+
+        setResult(MainActivity.REQUEST, intent);
+
+        finish();//finishing activity
+
     }
 }
