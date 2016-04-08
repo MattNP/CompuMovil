73905160df416e2381712bc5d7c34356bc5cace3
Mateo Noreña <npmatt23@gmail.com>
2016-04-08 16:25:38 -0500

    modificaciones esteticas

--- GeoLaps/app/src/main/java/co/edu/udea/compumovil/gr4/geolaps/Dashboard.java
+++ GeoLaps/app/src/main/java/co/edu/udea/compumovil/gr4/geolaps/Dashboard.java
@@ -87,20 +87,11 @@ public class Dashboard extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.perfil_usuario) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.my_place) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
