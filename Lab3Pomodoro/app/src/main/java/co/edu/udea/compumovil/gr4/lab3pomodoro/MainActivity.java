3444fa7de2a51bcb29c47b89ca38a95db6965b10
Mateo Noreña <npmatt23@gmail.com>
2016-04-12 22:52:01 -0500

    Avance Configuración y Material Design

--- Lab3Pomodoro/app/src/main/java/co/edu/udea/compumovil/gr4/lab3pomodoro/MainActivity.java
+++ Lab3Pomodoro/app/src/main/java/co/edu/udea/compumovil/gr4/lab3pomodoro/MainActivity.java
@@ -1,18 +1,30 @@
 package co.edu.udea.compumovil.gr4.lab3pomodoro;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_startPomodoro;
+    private static final int RESULT_SETTINGS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
+
+
         btn_startPomodoro = (Button)findViewById(R.id.btn_startPomodoro);
     }
 
@@ -27,5 +39,37 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_configuracion, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menu_settings:
+                Intent i = new Intent(this, OpcionesActivity.class);
+                startActivityForResult(i, RESULT_SETTINGS);
+                break;
+
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SETTINGS:
+                break;
+
+        }
+
     }
 }
--- Lab3Pomodoro/app/src/main/java/co/edu/udea/compumovil/gr4/lab3pomodoro/OpcionesActivity.java
+++ Lab3Pomodoro/app/src/main/java/co/edu/udea/compumovil/gr4/lab3pomodoro/OpcionesActivity.java
@@ -1,13 +1,27 @@
 package co.edu.udea.compumovil.gr4.lab3pomodoro;
 
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OpcionesActivity extends AppCompatActivity {
+public class OpcionesActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opciones);
+        //getFragmentManager().beginTransaction().replace(android.R.id.content, new OpcionesFragment()).commit();
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+
+    public static class OpcionesFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
     }
 }
